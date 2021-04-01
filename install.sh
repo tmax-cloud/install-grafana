d6a0ce5a08dedd832bae50c65c260ba4bf7b9e83
bongwoobak <66110096+bongwoobak@users.noreply.github.com>
2021-04-01 15:10:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
-sudo docker pull grafana/grafana:${GRAFANA_VERSION}
-sudo docker save grafana/grafana:${GRAFANA_VERSION} > grafana_${GRAFANA_VERSION}.tar
+sudo chmod +x version.conf
+source version.conf
 
-sudo docker load < grafana_${GRAFANA_VERSION}.tar
-
-sudo docker tag grafana/grafana:${GRAFANA_VERSION} ${REGISTRY}/grafana:${GRAFANA_VERSION}
-
-sudo docker push ${REGISTRY}/grafana:${GRAFANA_VERSION}
+cd yaml
+if [ $REGISTRY != "{REGISTRY}" ]; then
+  sudo docker tag grafana/grafana:${GRAFANA_VERSION} ${REGISTRY}/grafana:${GRAFANA_VERSION}
+  sudo docker push ${REGISTRY}/grafana:${GRAFANA_VERSION}
+  sed -i "s/grafana\/grafana/${REGISTRY}\/grafana/g" grafana.yaml
+ fi
 
 
 sed -i 's/{GRAFANA_VERSION}/'${GRAFANA_VERSION}'/g' grafana.yaml
 
-sed -i "s/grafana\/grafana/${REGISTRY}\/grafana/g" grafana.yaml
+
 
 kubectl create -f yaml/
