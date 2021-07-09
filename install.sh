5063d4695a44171ce7c4fe528b26d1300905f14f
bongwoobak <66110096+bongwoobak@users.noreply.github.com>
2021-07-09 14:24:50 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ sed -i 's/{DOMAIN}/'${DOMAIN}'/g' grafana-config.yaml
 sed -i 's/{CLIENT_ID}/'${CLIENT_ID}'/g' grafana-config.yaml
 sed -i 's/{CLIENT_SECRET}/'${CLIENT_SECRET}'/g' grafana-config.yaml
 sed -i 's/{KEYCLOAK_ADDR}/'${KEYCLOAK_ADDR}'/g' grafana-config.yaml
+sed -i 's/{GRAFANA_PVC}/'${GRAFANA_PVC}'/g' grafana-pvc.yaml
 
 
 cd ..
